5ad4d51bb52570cd71c39066937cbe5500367ae8
Artur <54704039+Artur-I@users.noreply.github.com>
2020-10-30 15:56:09 +0300

    feat: add four solutions

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let arr = matrix.reduce((acc, val) => [...acc, ...val], [])
+  //let count = arr.reduce((acc, cats) => acc + (acc[cats] !== '^^' ? 0 : acc[cats] + 1), 0)
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '^^') {
+      count++
+    }
+  }
+  return count
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(Array.isArray(members))) return false
+  let arr = []
+  for (let member of members) {
+    if (typeof member === 'string') {
+      arr.push(member.trim().charAt(0).toUpperCase())
+    }
+  }
+  return arr.sort().join('')
 };
